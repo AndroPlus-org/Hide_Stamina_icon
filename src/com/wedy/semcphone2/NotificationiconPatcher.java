027c2541ddf20314866d87b28391b2391aec78cf
WedyDQ10 <urobuthi@gmail.com>
2013-10-30 08:58:14 +0900

    Changed the code

--- src/com/wedy/semcphone2/NotificationiconPatcher.java
+++ src/com/wedy/semcphone2/NotificationiconPatcher.java
@@ -1,7 +1,6 @@
 package com.wedy.semcphone2;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
@@ -9,22 +8,23 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage.LoadPackageParam;
 public class NotificationiconPatcher implements IXposedHookLoadPackage {
 
 
-    @Override
-    public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
-        if (lpparam.packageName.equals("com.sonymobile.superstamina")) {
-            ClassLoader classLoader = lpparam.classLoader;
-            XC_MethodReplacement methodreplacer = new XC_MethodReplacement() {
-                protected Object replaceHookedMethod(
-                        XC_MethodHook.MethodHookParam paramAnonymousMethodHookParam)
-                        throws Throwable {
-                    return null;
-                }
-            };
-            XposedHelpers.findAndHookMethod("com.sonymobile.superstamina.XperiaPowerService",
-                    classLoader, "updateStatusbarIcon", methodreplacer);
-        }
+    	@Override
+	public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
 
+		if (!lpparam.packageName.equals("com.sonymobile.superstamina"))
+			return;
 
-    }
+		try {
+			XposedHelpers.findAndHookMethod(
+					"com.sonymobile.superstamina.XperiaPowerService",
+					lpparam.classLoader,
+					"updateStatusbarIcon",
+					XC_MethodReplacement.DO_NOTHING
+			);
+		} catch (Throwable t) {
+			
+		}
+
+	};
 
 }
